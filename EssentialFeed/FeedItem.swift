e4ad63269ad31c71a7bc89e2bc8d9e66455376b2
Alex Chen <alex851007@gmail.com>
2021-12-20 12:21:30 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by shuo on 2021/12/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
