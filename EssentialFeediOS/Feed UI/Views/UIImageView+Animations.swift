9b6da687c0b28b58ffab1beed4d63a6e26576b53
Alex Chen <alex851007@gmail.com>
2022-08-25 17:49:40 +0800

    Display image with animation

---
+++ EssentialFeediOS/Feed UI/Views/UIImageView+Animations.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+Animation.swift
+//  EssentialFeediOS
+//
+//  Created by shuo on 2022/8/25.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
