bbe494e7218db678e49b57b71d5e8113f6a45428
Alex Chen <alex851007@gmail.com>
2022-08-31 17:49:41 +0800

    Rename test case class to reflect the use case it refers to

--- EssentialFeedTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedImageDataLoaderTests.swift
+//  LoadFeedImageDataFromRemoteUseCaseTests.swift
 //  EssentialFeedTests
 //
 //  Created by shuo on 2022/8/31.
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedImageDataLoaderTests: XCTestCase {
+class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
